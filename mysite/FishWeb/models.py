856df5fc53d81a4666de51c5605cd9ea34bd0467
Christian <CAlfano1999@gmail.com>
2020-12-23 21:46:34 -0500

    added proper looking card products for displaying the items in stock along with price and quantity. Added a discount price feature as well. Added store hours along with other information.

--- mysite/FishWeb/models.py
+++ mysite/FishWeb/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     quantity = models.IntegerField()
     image = models.ImageField()
 
--- mysite/FishWeb/urls.py
+++ mysite/FishWeb/urls.py
@@ -6,8 +6,8 @@ app_name = 'FishWeb'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('products/', views.ProductView.as_view(), name='products'),
+    # path('products/', views.ProductView.as_view(), name='products'),
     path('store/', views.StoreView.as_view(), name='store'),
-    path('product/', views.FishView.as_view(), name ='product')
+    path('products/', views.FishView.as_view(), name ='products')
 
 ]
